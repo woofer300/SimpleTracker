be2ca4982f88cea53c1176c850b19f728bdf5a42
Chris Voon <christopher.voon@gmail.com>
2024-01-01 11:57:36 -0800

    Remove broken attempts to add ChatColor to console info logs

--- src/main/java/dev/woofer/simpletracker/SimpleTracker.java
+++ src/main/java/dev/woofer/simpletracker/SimpleTracker.java
@@ -1,19 +1,18 @@
 package dev.woofer.simpletracker;
 
 import dev.woofer.simpletracker.commands.CommandCoords;
-import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SimpleTracker extends JavaPlugin {
     @Override
     public void onDisable() {
-        this.getLogger().info(ChatColor.RED + "SimpleTracker has been disabled!");
+        this.getLogger().info("SimpleTracker has been disabled!");
     }
 
     @Override
     public void onEnable() {
         this.getLogger().info("Starting SimpleTracker...");
         this.getCommand("coords").setExecutor(new CommandCoords());
-        this.getLogger().info(ChatColor.GREEN + "SimpleTracker has been enabled!");
+        this.getLogger().info("SimpleTracker has been enabled!");
     }
 }
